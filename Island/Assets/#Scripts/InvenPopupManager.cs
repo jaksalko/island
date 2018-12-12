b29fd59c24ee6cf9fe5fb1766e759f066678c7aa
jaksalko <jaksal95@nate.com>
2018-12-12 17:02:49 +0900

    Image update

---
+++ Island/Assets/#Scripts/InvenPopupManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InvenPopupManager : MonoBehaviour {
+    public GameObject invenPopup;
+    public GameObject matPop;
+    public GameObject toolPop;
+    public Button exitbutton1;
+    public Button exitbutton2;
+
+    // Use this for initialization
+    public void ExitButtonClick() {
+        invenPopup.SetActive(false);
+    }
+    public void MatClick() {
+        toolPop.SetActive(false);
+        matPop.SetActive(true);
+    }
+    public void ToolClick()
+    {
+        toolPop.SetActive(true);
+        matPop.SetActive(false);
+    }
+}

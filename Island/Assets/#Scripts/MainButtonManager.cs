4228222084a60c103952e57057558895453aebd0
jaksalko <jaksal95@nate.com>
2019-01-21 16:14:32 +0900

    Moons Update

--- Island/Assets/#Scripts/MainButtonManager.cs
+++ Island/Assets/#Scripts/MainButtonManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-public class MainButtonManager : MonoBehaviour {
+public class MainButtonManager : MonoBehaviour
+{
     public Button[] peopleicon;
     public Button[] people;
     public Button[] icon;
@@ -13,20 +14,20 @@ public class MainButtonManager : MonoBehaviour {
     public GameObject[] statPopup;
     public GameObject workPopup;
     public GameObject tradePopup;
-
+    public GameObject nightPopup;
     public TextMeshProUGUI[] WorkNameText;
 
-    string[] Work = { "옷만들기", "배만들기","요리하기","낚시하기","농사짓기","집짓기","통신장치제작","제약하기","수렵하기", "탐험하기" };
+    string[] Work = { "옷만들기", "배만들기", "요리하기", "낚시하기", "농사짓기", "집짓기", "통신장치제작", "제약하기", "수렵하기", "탐험하기" };
     //string menuanimator;
     void Start()
     {
-        
-       // menuanimator = gameObject.GetComponent<MenuAnimator>().state;
+
+        // menuanimator = gameObject.GetComponent<MenuAnimator>().state;
     }
 
     public void PeopleiconClick(Button button)
     {
-        if ((statPopup[0].activeSelf == false&& statPopup[1].activeSelf == false&& statPopup[2].activeSelf == false))
+        if ((statPopup[0].activeSelf == false && statPopup[1].activeSelf == false && statPopup[2].activeSelf == false))
         {
             switch (button.ToString())
             {
@@ -37,11 +38,11 @@ public class MainButtonManager : MonoBehaviour {
                     statPopup[1].SetActive(true);
                     break;
                 case ("StatButton3 (UnityEngine.UI.Button)"):
-                    
+
                     statPopup[2].SetActive(true);
                     break;
             }
-           
+
         }
         else if (statPopup[0].activeSelf == true)
         {
@@ -49,7 +50,7 @@ public class MainButtonManager : MonoBehaviour {
             switch (button.ToString())
             {
                 case ("StatButton1 (UnityEngine.UI.Button)"):
-                   
+
                     break;
                 case ("StatButton2 (UnityEngine.UI.Button)"):
                     statPopup[1].SetActive(true);
@@ -61,7 +62,7 @@ public class MainButtonManager : MonoBehaviour {
         }
         else if (statPopup[1].activeSelf == true)
         {
-            
+
             statPopup[1].SetActive(false);
             switch (button.ToString())
             {
@@ -69,7 +70,7 @@ public class MainButtonManager : MonoBehaviour {
                     statPopup[0].SetActive(true);
                     break;
                 case ("StatButton2 (UnityEngine.UI.Button)"):
-                    
+
                     break;
                 case ("StatButton3 (UnityEngine.UI.Button)"):
                     statPopup[2].SetActive(true);
@@ -88,7 +89,7 @@ public class MainButtonManager : MonoBehaviour {
                     statPopup[1].SetActive(true);
                     break;
                 case ("StatButton3 (UnityEngine.UI.Button)"):
-                    
+
                     break;
             }
         }
@@ -126,26 +127,26 @@ public class MainButtonManager : MonoBehaviour {
     }
     public void PeopleClick(Button button)
     {
-        
-        
+
+
         switch (button.ToString())//각각 사람의 이미지는 MyCharacterData의 CharacterName에 있는 순서대로 해야함! 
         {
-            
+
             case ("People1 (UnityEngine.UI.Button)"):
                 {
 
-                    MyCharacterData.Instance.NowCharacterName = MyCharacterData.Instance.ThreeCharacter[0].Name-1;
+                    MyCharacterData.Instance.NowCharacterName = MyCharacterData.Instance.ThreeCharacter[0].Name - 1;
                     for (int i = 0; i < WorkNameText.Length; i++)
                     {
-                        
-                            WorkNameText[i].text = Work[i].ToString();
+
+                        WorkNameText[i].text = Work[i].ToString();
 
                     }
                     break;
                 }
             case ("People2 (UnityEngine.UI.Button)"):
                 {
-                    MyCharacterData.Instance.NowCharacterName = MyCharacterData.Instance.ThreeCharacter[1].Name-1;
+                    MyCharacterData.Instance.NowCharacterName = MyCharacterData.Instance.ThreeCharacter[1].Name - 1;
                     for (int i = 0; i < WorkNameText.Length; i++)
                     {
 
@@ -156,7 +157,7 @@ public class MainButtonManager : MonoBehaviour {
                 }
             case ("People3 (UnityEngine.UI.Button)"):
                 {
-                    MyCharacterData.Instance.NowCharacterName = MyCharacterData.Instance.ThreeCharacter[2].Name-1;
+                    MyCharacterData.Instance.NowCharacterName = MyCharacterData.Instance.ThreeCharacter[2].Name - 1;
                     for (int i = 0; i < WorkNameText.Length; i++)
                     {
 
@@ -169,7 +170,7 @@ public class MainButtonManager : MonoBehaviour {
         Debug.Log("캐릭터 번호" + MyCharacterData.Instance.NowCharacterName);
         workPopup.SetActive(true);
 
-        
+
 
     }
     public void IconClick(Button button)
@@ -177,22 +178,24 @@ public class MainButtonManager : MonoBehaviour {
         switch (button.ToString())
         {
             case ("MissionButton (UnityEngine.UI.Button)"):
-                
+
                 break;
             case ("TradeButton (UnityEngine.UI.Button)"):
                 tradePopup.SetActive(true);
                 break;
             case ("MenuButton (UnityEngine.UI.Button)"):
 
-                if (MenuAnimator.Instance.state != "triggerOn") {
+                if (MenuAnimator.Instance.state != "triggerOn")
+                {
                     MenuAnimator.Instance.state = "triggerOn";
-                } else if (MenuAnimator.Instance.state == "triggerOn")
-                        {
+                }
+                else if (MenuAnimator.Instance.state == "triggerOn")
+                {
                     MenuAnimator.Instance.state = "triggerOff";
-                         }
-                    
-                
-                    
+                }
+
+
+
 
                 break;
             case ("InvenButton (UnityEngine.UI.Button)"):
@@ -200,6 +203,10 @@ public class MainButtonManager : MonoBehaviour {
                 break;
             case ("ItemButton (UnityEngine.UI.Button)"):
                 manufactPopup.SetActive(true);
+                break;
+
+            case ("NightButton (UnityEngine.UI.Button)"):
+                nightPopup.SetActive(true);
                 break;
         }
     }

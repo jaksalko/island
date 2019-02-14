869fcf419ec80c1def14d316c6530b9c5f6b5b93
jaksalko <jaksal95@nate.com>
2019-02-14 15:46:37 +0900

    fishdungeon

---
+++ Island/Assets/#Scripts/FishDungeonManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FishDungeonManager : MonoBehaviour {
+
+    public static string dungeonLv;
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+    public void FishDungeonClicked()
+    {
+        dungeonLv = EventSystem.current.currentSelectedGameObject.name;
+        dungeonLv = dungeonLv.Replace("Dungeon", "");
+        AutoFade.LoadLevel("FishGame", 1, 1, Color.black);
+    }
+    public void EnterDungeonButton()
+    {
+       
+    }
+}
